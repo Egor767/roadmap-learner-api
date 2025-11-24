fc70db1f6b59e9ee1655898d45913b937de2b48e
Egor Musiyak <musiyak26@mail.ru>
2025-11-24 18:30:08 +0600

    delete resource-routers

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -6,14 +6,8 @@ from fastapi.security import HTTPBearer
 
 from app.core.config import settings
 from .auth import router as auth_router
-from .block import (
-    router as block_router,
-    resource_router as block_resource_router,
-)
-from .card import (
-    router as card_router,
-    resource_router as card_resource_router,
-)
+from .block import router as block_router
+from .card import router as card_router
 from .roadmap import router as roadmap_router
 from .session import router as session_manager_router
 from .user import router as user_router
@@ -29,7 +23,5 @@ router.include_router(user_router)
 
 router.include_router(roadmap_router)
 router.include_router(block_router)
-router.include_router(block_resource_router)
 router.include_router(card_router)
-router.include_router(card_resource_router)
 router.include_router(session_manager_router)
